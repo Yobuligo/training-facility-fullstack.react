040f2233a8435ff81e7e6fde7647c23489987626
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-28 16:03:42 +0200

    Style UserProfileItems for deactivated UserProfile

--- frontend/src/features/users/userProfileItem/UserProfileItem.tsx
+++ frontend/src/features/users/userProfileItem/UserProfileItem.tsx
@@ -1,22 +1,31 @@
 import { Card } from "../../../components/card/Card";
+import { texts } from "../../../hooks/useTranslation/texts";
+import { useTranslation } from "../../../hooks/useTranslation/useTranslation";
 import { style } from "../../../utils/style";
 import { UserProfile } from "../userProfile/UserProfile";
 import { IUserProfileItem } from "./IUserProfileItemProps";
 import styles from "./UserProfileItem.module.scss";
 
 export const UserProfileItem: React.FC<IUserProfileItem> = (props) => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <Card
         className={style(
           styles.userProfileItem,
+          props.userProfile.isDeactivated ? styles.deactivated : "",
           props.isSelected ? styles.selected : ""
         )}
         onClick={props.onSelect}
       >
         <div className={styles.group}>
           <div className={styles.name}>
-            {`${props.userProfile.firstname} ${props.userProfile.lastname}`}
+            {`${props.userProfile.firstname} ${props.userProfile.lastname} ${
+              props.userProfile.isDeactivated === true
+                ? `(${t(texts.userProfileItem.deactivated)})`
+                : ""
+            }`}
           </div>
           <div className={styles.address}>
             {`${props.userProfile.street} ${props.userProfile.postalCode} ${props.userProfile.city}`}
