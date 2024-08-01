0dc7876717fd6e5e5fb603c2dfb6f9d39c12133f
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-01 12:39:50 +0200

    Realign app logo

--- frontend/src/components/pages/pageHeader/PageHeader.tsx
+++ frontend/src/components/pages/pageHeader/PageHeader.tsx
@@ -5,7 +5,6 @@ import styles from "./PageHeader.module.scss";
 export const PageHeader: React.FC<IPageHeaderProps> = (props) => {
   return (
     <header className={styles.pageHeader}>
-      <div></div>
       <div>
         <AppLogo className={styles.appLogo} onClick={props.onAppLogoClick} />
       </div>
