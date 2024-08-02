e0dee26548f5e5834c8a385574c2d4d3abce32a3
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-02 22:46:00 +0200

    Display message in grading list, if no grading is available

--- frontend/src/features/grading/gradingList/GradingList.tsx
+++ frontend/src/features/grading/gradingList/GradingList.tsx
@@ -1,9 +1,13 @@
+import { texts } from "../../../hooks/useTranslation/texts";
+import { useTranslation } from "../../../hooks/useTranslation/useTranslation";
 import { DateTime } from "../../../services/date/DateTime";
 import { GradingItem } from "../gradingItem/GradingItem";
 import styles from "./GradingList.module.scss";
 import { IGradingListProps } from "./IGradingListProps";
 
 export const GradingList: React.FC<IGradingListProps> = (props) => {
+  const { t } = useTranslation()
+
   const items = props.gradings
     .sort((left, right) => DateTime.compare(right.achievedAt, left.achievedAt))
     .map((grading) => (
@@ -16,5 +20,5 @@ export const GradingList: React.FC<IGradingListProps> = (props) => {
       />
     ));
 
-  return <div className={styles.gradingList}>{items}</div>;
+  return <div className={styles.gradingList}>{items.length > 0 ? items : t(texts.gradingList.noGradingsAvailable)}</div>;
 };
