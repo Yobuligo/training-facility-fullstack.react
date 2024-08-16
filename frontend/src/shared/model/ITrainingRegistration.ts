e97acef7c0f43fff37e095ff3e17bfb9c0117866
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-16 10:11:43 +0200

    Extend model for trainings

--- frontend/src/shared/model/ITrainingRegistration.ts
+++ frontend/src/shared/model/ITrainingRegistration.ts
@@ -1,13 +1,13 @@
 import { IEntity } from "../types/IEntity";
 import { IHaveUserId } from "../types/IHaveUserId";
 import { TrainingInstanceState } from "../types/TrainingInstanceState";
-import { ITrainingInstance } from "./ITrainingInstance";
+import { ITrainingSession } from "./ITrainingSession";
 
 /**
  * This interface represents a specific registration of a user on a trainings instance
  */
 export interface ITrainingRegistration extends IEntity, IHaveUserId {
-  trainingsInstance: ITrainingInstance;
-  trainingsInstanceId: string;
+  trainingsSession: ITrainingSession;
+  trainingsSessionId: string;
   state: TrainingInstanceState;
 }
--- frontend/src/shared/model/ITrainingSession.ts
+++ frontend/src/shared/model/ITrainingSession.ts
@@ -3,9 +3,9 @@ import { IEntity } from "../types/IEntity";
 import { ITrainingDefinition } from "./ITrainingDefinition";
 
 /**
- * This interface represents a specific training instance based on a training definition
+ * This interface represents a specific training session based on a training definition
  */
-export interface ITrainingInstance extends IEntity, IDateTimeSpan {
+export interface ITrainingSession extends IEntity, IDateTimeSpan {
   subTitle: string;
   title: string;
   trainingDefinition: ITrainingDefinition;
