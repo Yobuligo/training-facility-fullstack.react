d86dc4599c1f65e32fa119c9b052cf6c169ee754
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-18 13:35:22 +0200

    Delete IAppointment

--- frontend/src/components/appointmentForm/model/IAppointment.ts
+++
@@ -1,9 +0,0 @@
-import { IDateTimeSpan } from "../../../core/services/date/IDateTimeSpan";
-import { Recurrence } from "../../../shared/types/Recurrence";
-
-export interface IAppointment extends IDateTimeSpan {
-  id: string;
-  description: string;
-  recurrence: Recurrence;
-  title: string;
-}
