8277958864786ac710c0ffa5b3c87cf24cc08ee1
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-21 10:50:11 +0200

    Display weekday in EventInstanceList

--- frontend/src/features/eventInstance/eventInstanceItem/EventInstanceItem.tsx
+++ frontend/src/features/eventInstance/eventInstanceItem/EventInstanceItem.tsx
@@ -1,9 +1,12 @@
 import { Card } from "../../../components/card/Card";
 import { DateTime } from "../../../core/services/date/DateTime";
+import { useRenderWeekday } from "../../../hooks/useRenderWeekday";
 import styles from "./EventInstanceItem.module.scss";
 import { IEventInstanceItemProps } from "./IEventInstanceItemProps";
 
 export const EventInstanceItem: React.FC<IEventInstanceItemProps> = (props) => {
+  const renderWeekday = useRenderWeekday();
+
   return (
     <Card className={styles.eventInstanceItem}>
       <div
@@ -14,6 +17,9 @@ export const EventInstanceItem: React.FC<IEventInstanceItemProps> = (props) => {
       />
       <div className={styles.content}>
         <div className={styles.title}>{props.eventInstance.title}</div>
+        <div className={styles.weekday}>
+          {renderWeekday(DateTime.toWeekday(props.eventInstance.from))}
+        </div>
         <div>{DateTime.format(props.eventInstance.from, "dd.MM.yyyy")}</div>
         <div>{`${DateTime.toTime(props.eventInstance.from)} - ${DateTime.toTime(
           props.eventInstance.to
