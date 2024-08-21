1dd5b75d307358950092db4f053e066e1b0cec5f
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-21 08:46:50 +0200

    Restyle EventInstanceImte

--- frontend/src/features/eventInstance/eventInstanceItem/EventInstanceItem.tsx
+++ frontend/src/features/eventInstance/eventInstanceItem/EventInstanceItem.tsx
@@ -1,41 +1,24 @@
 import { Card } from "../../../components/card/Card";
-import { LabeledInput } from "../../../components/labeledInput/LabeledInput";
 import { DateTime } from "../../../core/services/date/DateTime";
-import { texts } from "../../../hooks/useTranslation/texts";
-import { useTranslation } from "../../../hooks/useTranslation/useTranslation";
-import { style } from "../../../utils/style";
 import styles from "./EventInstanceItem.module.scss";
 import { IEventInstanceItemProps } from "./IEventInstanceItemProps";
 
 export const EventInstanceItem: React.FC<IEventInstanceItemProps> = (props) => {
-  const { t } = useTranslation();
-
   return (
     <Card className={styles.eventInstanceItem}>
-      <LabeledInput
-        classNameInput={style(styles.input, styles.title)}
-        label={t(texts.eventInstanceItem.title)}
-        value={props.eventInstance.title}
+      <div
+        className={styles.banner}
+        style={{
+          backgroundColor: `${props.eventInstance.eventDefinition.color}`,
+        }}
       />
-      <LabeledInput
-        classNameInput={styles.input}
-        label={t(texts.eventInstanceItem.date)}
-        type="date"
-        value={DateTime.toDate(props.eventInstance.from)}
-      />
-      <LabeledInput
-        classNameInput={styles.input}
-        label={t(texts.eventInstanceItem.startAtTime)}
-        type="time"
-        value={DateTime.toTime(props.eventInstance.from)}
-      />
-      <div className={styles.separator} />
-      <LabeledInput
-        classNameInput={styles.input}
-        label={t(texts.eventInstanceItem.endAtTime)}
-        type="time"
-        value={DateTime.toTime(props.eventInstance.to)}
-      />
+      <div className={styles.content}>
+        <div className={styles.title}>{props.eventInstance.title}</div>
+        <div>{DateTime.format(props.eventInstance.from, "dd.MM.yyyy")}</div>
+        <div>{`${DateTime.toTime(props.eventInstance.from)} - ${DateTime.toTime(
+          props.eventInstance.to
+        )}`}</div>
+      </div>
     </Card>
   );
 };
