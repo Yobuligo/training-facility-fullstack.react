87f0b8bfaf05bafa30fb2ff6bafbcaf741cf5413
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-22 16:55:20 +0200

    Style EventRegistrationItem

--- frontend/src/features/eventRegistration/eventRegistrationItem/EventRegistrationItem.tsx
+++ frontend/src/features/eventRegistration/eventRegistrationItem/EventRegistrationItem.tsx
@@ -1,4 +1,6 @@
+import { Button } from "../../../components/button/Button";
 import { Card } from "../../../components/card/Card";
+import { Toolbar } from "../../../components/toolbar/Toolbar";
 import { checkNotNull } from "../../../core/utils/checkNotNull";
 import styles from "./EventRegistrationItem.module.scss";
 import { IEventRegistrationItemProps } from "./IEventRegistrationItemProps";
@@ -13,6 +15,10 @@ export const EventRegistrationItem: React.FC<IEventRegistrationItemProps> = (
       <div className="styles.name">
         {`${userProfile.firstname} ${userProfile.lastname}`}
       </div>
+      <Toolbar>
+        <Button>Present</Button>
+        <Button>Missing</Button>
+      </Toolbar>
     </Card>
   );
 };
