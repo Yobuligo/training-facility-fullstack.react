9fa74d13accf77fd13c7287d44fc6b1f3d41c0ab
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-22 21:35:26 +0200

    Add SecondaryButton as register button and centralize secondaryButton styling

--- frontend/src/components/secondaryButton/SecondaryButton.tsx
+++ frontend/src/components/secondaryButton/SecondaryButton.tsx
@@ -1,7 +1,7 @@
+import componentStyles from "../../styles/components.module.scss";
 import { Button } from "../button/Button";
 import { ISecondaryButtonProps } from "./ISecondaryButtonProps";
-import styles from "./SecondaryButton.module.scss";
 
 export const SecondaryButton: React.FC<ISecondaryButtonProps> = (props) => {
-  return <Button {...props} className={styles.secondaryButton} />;
+  return <Button {...props} className={componentStyles.secondaryButton} />;
 };
