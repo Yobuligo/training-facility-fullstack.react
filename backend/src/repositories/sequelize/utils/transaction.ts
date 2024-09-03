1cc036f83482906f30e89e151c4ad249664b3381
Yobuligo <hoffmannpeter82@gmail.com>
2024-09-03 07:39:26 +0200

    Restrict transaction to one

--- backend/src/repositories/sequelize/utils/transaction.ts
+++ backend/src/repositories/sequelize/utils/transaction.ts
@@ -7,12 +7,21 @@ import { TransactionStack } from "./TransactionStack";
  * This function creates a Sequelize transaction and wraps the method calls of {@link block} with that transaction.
  * Within these blocks the transaction can be retrieved via {@link findTransaction}.
  * There is no need to commit the transaction, as it is committed automatically.
+ * Rolls back the transaction in case of errors
  *
- * Throws errors and rolls back changes.
+ * If the function is called inside of itself, the changes are belonging to the outer transaction. So no new transaction is created.
  */
 export const transaction = async (
   block: (transaction: Transaction) => Promise<void>
 ) => {
+  // check if a transaction was already created and just call block instead of creating a new transaction
+  const transaction = findTransaction();
+  if (transaction) {
+    block(transaction);
+    return;
+  }
+
+  // otherwise create a new transaction
   try {
     await db.transaction(async (transaction) => {
       TransactionStack.instance.push(transaction);
