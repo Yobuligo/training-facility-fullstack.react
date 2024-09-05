9ca1e1f49b00a2403d2904e691b80134b2bda4a5
Yobuligo <hoffmannpeter82@gmail.com>
2024-09-05 11:24:58 +0200

    Create db join to select EventDefinitions and EventInstances

--- backend/src/controllers/EventDefinitionController.ts
+++ backend/src/controllers/EventDefinitionController.ts
@@ -22,16 +22,24 @@ export class EventDefinitionController extends EntityController<
         const fields = this.getFieldsFromQuery(req.query);
         const from = req.query.from;
         const to = req.query.to;
+        const userId = req.query.userId;
 
         if (from && typeof from === "string" && to && typeof to === "string") {
           const dateTimeSpan: IDateTimeSpan = {
             from: new Date(from),
             to: new Date(to),
           };
-          const entities = await this.repo.findByDateTimeSpan(
-            dateTimeSpan,
-            fields
-          );
+
+          let entities: IEventDefinition[];
+          if (userId && typeof userId === "string") {
+            entities = await this.repo.findByDateTimeSpanAndUser(
+              dateTimeSpan,
+              userId,
+              fields
+            );
+          } else {
+            entities = await this.repo.findByDateTimeSpan(dateTimeSpan, fields);
+          }
           res.status(200).send(entities);
         } else {
           const entities = await this.repo.findAll(fields);
--- backend/src/repositories/EventDefinitionRepo.ts
+++ backend/src/repositories/EventDefinitionRepo.ts
@@ -1,18 +1,35 @@
+import sequelize from "sequelize";
 import { IEntitySubset } from "../core/api/types/IEntitySubset";
 import { DateTime } from "../core/services/date/DateTime";
-import { DateTimeIterator } from "../core/services/date/DateTimeIterator";
 import { IDateTimeSpan } from "../core/services/date/IDateTimeSpan";
-import { Lazy } from "../core/services/Lazy";
 import { List } from "../core/services/list/List";
-import { Recurrence } from "../core/types/Recurrence";
+import { db } from "../db/db";
 import { EventDefinition } from "../model/EventDefinition";
 import { EventInstance } from "../model/EventInstance";
 import { IEventDefinition } from "../shared/model/IEventDefinition";
+import { IEventInstance } from "../shared/model/IEventInstance";
 import { SequelizeRepository } from "./sequelize/SequelizeRepository";
 
 export class EventDefinitionRepo extends SequelizeRepository<IEventDefinition> {
   constructor() {
     super(EventDefinition, [{ model: EventInstance, as: "eventInstances" }]);
+  }
+
+  findByDateTimeSpanAndUser<K extends keyof IEventDefinition>(
+    dateTimeSpan: IDateTimeSpan,
+    userId: string,
+    fields: K[]
+  ): Promise<IEntitySubset<IEventDefinition, K>[]>;
+  findByDateTimeSpanAndUser(
+    dateTimeSpan: IDateTimeSpan,
+    userId: string
+  ): Promise<IEventDefinition[]>;
+  async findByDateTimeSpanAndUser(
+    dateTimeSpan: IDateTimeSpan,
+    userId: string,
+    fields?: unknown
+  ): Promise<unknown> {
+    return await this.selectEventDefinitionsAndInstances(dateTimeSpan);
   }
 
   findByDateTimeSpan<K extends keyof IEventDefinition>(
@@ -24,125 +41,16 @@ export class EventDefinitionRepo extends SequelizeRepository<IEventDefinition> {
     dateTimeSpan: IDateTimeSpan,
     fields?: unknown
   ): Promise<unknown> {
-    // filter data definition
-    const data = await this.model.findAll();
-    const eventDefinitions = data.map((model) => model.toJSON());
-    let filteredEventDefinitions = this.filterEventDefinition(
-      dateTimeSpan,
-      eventDefinitions
-    );
+    let eventDefinitions = await this.selectEventDefinitions(dateTimeSpan);
 
     // restrict properties to given fields
     const keyFields = this.getKeyFields(fields);
     if (List.isNotEmpty(keyFields)) {
-      filteredEventDefinitions = filteredEventDefinitions.map(
-        (filteredEventDefinition) =>
-          this.restrictFields(filteredEventDefinition, keyFields)
+      eventDefinitions = eventDefinitions.map((eventDefinition) =>
+        this.restrictFields(eventDefinition, keyFields)
       );
     }
-    return filteredEventDefinitions;
-  }
-
-  private filterEventDefinition(
-    dateTimeSpan: IDateTimeSpan,
-    eventDefinitions: IEventDefinition[]
-  ): IEventDefinition[] {
-    if (List.isEmpty(eventDefinitions)) {
-      return [];
-    }
-
-    const lazyWeekdays = this.createLazyWeekdays(dateTimeSpan);
-    const lazyDays = this.createLazyDays(dateTimeSpan);
-
-    const filteredEventDefinitions = eventDefinitions.filter(
-      (eventDefinition) => {
-        // does event definition of recurrence type "once" matches the range?
-        if (eventDefinition.recurrence === Recurrence.ONCE) {
-          return (
-            DateTime.toDate(eventDefinition.from) >=
-              DateTime.toDate(dateTimeSpan.from) &&
-            DateTime.toDate(eventDefinition.to) <=
-              DateTime.toDate(dateTimeSpan.to)
-          );
-        }
-
-        // does event definition of recurrence type "daily" matches the range?
-        if (eventDefinition.recurrence === Recurrence.DAILY) {
-          if (
-            !this.matchesDateTimeSpan(
-              dateTimeSpan.from,
-              dateTimeSpan.to,
-              eventDefinition
-            )
-          ) {
-            return false;
-          }
-          return true;
-        }
-
-        // does event definition of recurrence type "week" matches the range?
-        if (eventDefinition.recurrence === Recurrence.WEEKLY) {
-          if (
-            !this.matchesDateTimeSpan(
-              dateTimeSpan.from,
-              dateTimeSpan.to,
-              eventDefinition
-            )
-          ) {
-            return false;
-          }
-
-          // get weekday of event definition and check if the weekday belongs to the weekdays of the given dateTimeSpan
-          const weekday = DateTime.toWeekday(eventDefinition.from);
-          return lazyWeekdays.data.has(weekday);
-        }
-
-        if (eventDefinition.recurrence === Recurrence.MONTHLY) {
-          if (
-            !this.matchesDateTimeSpan(
-              dateTimeSpan.from,
-              dateTimeSpan.to,
-              eventDefinition
-            )
-          ) {
-            return false;
-          }
-
-          // get day of definition and check if the day belongs to the days of the given dateTimeSpan
-          const day = DateTime.toDay(eventDefinition.from);
-          return lazyDays.data.has(day);
-        }
-
-        return false;
-      }
-    );
-    return filteredEventDefinitions;
-  }
-
-  /**
-   * Build a list of weekdays for the given {@link dateTimeSpan}.
-   */
-  private createLazyWeekdays(dateTimeSpan: IDateTimeSpan): Lazy<Set<number>> {
-    return new Lazy(() => {
-      const weekdays = new Set<number>();
-      DateTimeIterator.iterate(dateTimeSpan.from, dateTimeSpan.to, (cursor) => {
-        weekdays.add(cursor.getDay());
-      });
-      return weekdays;
-    });
-  }
-
-  /**
-   * Build a list of days for the given {@link dateTimeSpan}.
-   */
-  private createLazyDays(dateTimeSpan: IDateTimeSpan): Lazy<Set<number>> {
-    return new Lazy(() => {
-      const days = new Set<number>();
-      DateTimeIterator.iterate(dateTimeSpan.from, dateTimeSpan.to, (cursor) => {
-        days.add(DateTime.toDay(cursor));
-      });
-      return days;
-    });
+    return eventDefinitions;
   }
 
   /**
@@ -173,5 +81,183 @@ export class EventDefinitionRepo extends SequelizeRepository<IEventDefinition> {
     }
 
     return true;
+  }
+
+  private async selectEventDefinitions(
+    dateTimeSpan: IDateTimeSpan
+  ): Promise<IEventDefinition[]> {
+    const query = `
+        WITH RECURSIVE date_range AS (
+            SELECT DATE(:from) AS datum
+            UNION ALL
+            SELECT DATE_ADD(datum, INTERVAL 1 DAY)
+            FROM date_range
+            WHERE datum < DATE(:to)
+        )
+
+        SELECT * FROM \`event-definitions\` WHERE 
+        # once
+        (recurrence = 0 AND (
+            DATE(\`from\`) >= DATE(:from) AND DATE(\`to\`) <= DATE(:to)
+        )) OR 
+
+        # daily
+        (recurrence = 1 AND (
+            DATE(\`from\`) <= DATE(:to) AND ( 
+                (DATE(\`to\`) = DATE(\`from\`)) OR (DATE(\`to\`) > DATE(:from))
+            )
+        )) OR 
+
+        # weekly
+        (recurrence = 2 AND (
+            DATE(\`from\`) <= DATE(:to) AND ( 
+                (DATE(\`to\`) = DATE(\`from\`)) OR (DATE(\`to\`) > DATE(:from))
+            ) AND
+            DAYOFWEEK(\`from\`) IN (SELECT DAYOFWEEK(datum) FROM date_range)
+        )) OR
+
+        # monthly
+        (recurrence = 3 AND (
+            DATE(\`from\`) <= DATE(:to) AND ( 
+                (DATE(\`to\`) = DATE(\`from\`)) OR (DATE(\`to\`) > DATE(:from))
+            ) AND
+            DAY(\`from\`) IN (SELECT DAY(datum) FROM date_range)
+        ));
+    `;
+
+    const eventDefinitions = await db.query<IEventDefinition>(query, {
+      replacements: {
+        from: dateTimeSpan.from,
+        to: dateTimeSpan.to,
+      },
+      type: sequelize.QueryTypes.SELECT,
+    });
+
+    return eventDefinitions;
+  }
+
+  private async selectEventDefinitionsAndInstances(
+    dateTimeSpan: IDateTimeSpan
+  ): Promise<IEventDefinition[]> {
+    const query = `
+        # get dates of range
+        WITH RECURSIVE date_range AS (
+            SELECT DATE(:from) AS datum
+            UNION ALL
+            SELECT DATE_ADD(datum, INTERVAL 1 DAY)
+            FROM date_range
+            WHERE datum < DATE(:to)
+        )
+
+        SELECT 
+          def.*,
+          inst.id AS inst_id,
+          inst.\`description\` AS inst_description,
+          inst.\`from\` AS inst_from,
+          inst.state AS inst_state,
+          inst.title AS inst_title,
+          inst.\`to\` AS inst_to,
+          inst.createdAt AS inst_createdAt,
+          inst.updatedAt AS inst_updatedAt,
+          inst.eventDefinitionId AS inst_eventDefinitionId
+        FROM \`event-definitions\` AS def
+        LEFT JOIN \`event-instances\` AS inst
+        ON def.id = inst.eventDefinitionId
+        WHERE 
+        # once
+        (recurrence = 0 && (
+          DATE(def.\`from\`) >= DATE(:from) && DATE(def.\`to\`) <= DATE(:to)
+        )) OR 
+
+        # daily
+        (recurrence = 1 && (
+          # matches given date time span
+          DATE(def.\`from\`) <= DATE(:to) && ( 
+            (DATE(def.\`to\`) = DATE(def.\`from\`)) OR (DATE(def.\`to\`) > DATE(:from))
+          )
+        )) OR 
+
+        # weekly
+        (recurrence = 2 AND (
+          # matches given date time span
+          (DATE(def.\`from\`) <= DATE(:to) AND ( 
+            (DATE(def.\`to\`) = DATE(def.\`from\`)) OR (DATE(def.\`to\`) > DATE(:from))
+          )) AND
+          
+          # matches the weekdays of the date time span
+          DAYOFWEEK(def.\`from\`) IN (SELECT DAYOFWEEK(datum) FROM date_range)
+        )) OR
+
+        # monthly
+        (recurrence = 3 && (
+          # matches given date time span
+          (DATE(def.\`from\`) <= DATE(:to) && ( 
+            (DATE(def.\`to\`) = DATE(def.\`from\`)) OR (DATE(def.\`to\`) > DATE(:from))
+          )) AND
+          
+          # matches the weekdays of the date time span
+          DAY(def.\`from\`) IN (SELECT DAY(datum) FROM date_range)
+        ))
+    `;
+
+    const data = await db.query<IEventDefinition>(query, {
+      replacements: {
+        from: dateTimeSpan.from,
+        to: dateTimeSpan.to,
+      },
+      type: sequelize.QueryTypes.SELECT,
+    });
+
+    return this.convertToEventDefinition(data);
+  }
+
+  private convertToEventDefinition(
+    data: IEventDefinition[]
+  ): IEventDefinition[] {
+    const eventDefinitionsDb: any = {};
+
+    data.forEach((row) => {
+      // create event definition, if not yet available
+      if (!eventDefinitionsDb[row.id]) {
+        const eventDefinition: IEventDefinition = {
+          id: row.id,
+          createdAt: row.createdAt,
+          updatedAt: row.updatedAt,
+          color: row.color,
+          creatorUserId: row.creatorUserId,
+          description: row.description,
+          from: row.from,
+          recurrence: row.recurrence,
+          title: row.title,
+          to: row.to,
+          eventInstances: [],
+        };
+        eventDefinitionsDb[row.id] = eventDefinition;
+      }
+
+      // create and add event instance if available
+      const rowAny: any = row;
+      if (rowAny.inst_id) {
+        const eventInstance: IEventInstance = {
+          id: rowAny.inst_id,
+          createdAt: rowAny.inst_createdAt,
+          updatedAt: rowAny.inst_updatedAt,
+          description: rowAny.inst_description,
+          eventDefinitionId: rowAny.inst_eventDefinitionId,
+          from: rowAny.inst_from,
+          state: rowAny.inst_state,
+          title: rowAny.inst_title,
+          to: rowAny.inst_to,
+          eventRegistrations: [],
+        };
+
+        (eventDefinitionsDb[row.id] as IEventDefinition).eventInstances?.push(
+          eventInstance
+        );
+      }
+    });
+
+    // convert to array
+    return Object.values(eventDefinitionsDb);
   }
 }
