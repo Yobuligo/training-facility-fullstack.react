e63c2234c690953058413e312559e6c0aa84f1e5
Yobuligo <hoffmannpeter82@gmail.com>
2024-09-10 10:32:24 +0200

    Clicking the Yeoljeong logo navigates to the dashboard

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { Error } from "../components/error/Error";
 import { PublicPage } from "../components/pages/publicPage/PublicPage";
+import { AppRoutes } from "../routes/AppRoutes";
 
 export const ErrorPage: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <PublicPage>
+    <PublicPage onAppLogoClick={() => navigate(AppRoutes.dashboard.toPath())}>
       <Error />
     </PublicPage>
   );
