c2e589b7ec368f08982e230f02742583a79eb14b
Yobuligo <hoffmannpeter82@gmail.com>
2024-09-16 14:11:20 +0200

    Make Login texts translatable

--- frontend/src/features/login/useLoginViewModel.ts
+++ frontend/src/features/login/useLoginViewModel.ts
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { isError } from "../../core/utils/isError";
-import { useToggle } from "../../hooks/useToggle";
 import { texts } from "../../lib/translation/texts";
 import { useTranslation } from "../../lib/translation/useTranslation";
 import { SessionRepo } from "../../lib/userSession/api/SessionRepo";
@@ -15,7 +14,6 @@ export const useLoginViewModel = () => {
   const [successMessage, setSuccessMessage] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [loginMode, toggleLoginMode] = useToggle(true);
   const [displaySpinner, setDisplaySpinner] = useState(false);
   const [, setSession] = useSession();
   const navigate = useNavigate();
@@ -24,23 +22,20 @@ export const useLoginViewModel = () => {
 
   const disablePassword = username.length === 0 && password.length === 0;
 
-  const resetErrorMessage = () => setErrorMessage("");
-
   const updateErrorMessage = (errorMessage: string) => {
     setErrorMessage(errorMessage);
     setSuccessMessage("");
   };
 
-  const updateSuccessMessage = (successMessage: string) => {
-    setErrorMessage("");
-    setSuccessMessage(successMessage);
-  };
-
-  const onConfirm = () => {
-    if (loginMode) {
-      onLogin();
+  const handleError = (error: any) => {
+    if (isError(error)) {
+      if (error.type === "InvalidCredentialsError") {
+        updateErrorMessage(t(texts.login.errorInvalidCredentials));
+      } else {
+        updateErrorMessage(error.message);
+      }
     } else {
-      onRegister();
+      updateErrorMessage(t(texts.login.errorLogin));
     }
   };
 
@@ -53,42 +48,15 @@ export const useLoginViewModel = () => {
       SessionRepo.instance.setSession(session);
       navigate(AppRoutes.dashboard.toPath());
     } catch (error) {
-      if (isError(error)) {
-        updateErrorMessage(error.message);
-      } else {
-        updateErrorMessage(t(texts.login.errorLogin));
-      }
+      handleError(error);
     }
     setDisplaySpinner(false);
   };
 
   const onEnter = () => {
     if (!disableLoginButton) {
-      onConfirm();
+      onLogin();
     }
-  };
-
-  const onRegister = async () => {
-    setDisplaySpinner(true);
-    try {
-      const userApi = new UserApi();
-      await userApi.register(username, password);
-      updateSuccessMessage(t(texts.login.successUserCreated));
-      toggleLoginMode(true);
-      setPassword("");
-    } catch (error) {
-      if (isError(error)) {
-        updateErrorMessage(error.message);
-      } else {
-        updateErrorMessage(t(texts.login.errorRegister));
-      }
-    }
-    setDisplaySpinner(false);
-  };
-
-  const onToggleLoginMode = () => {
-    toggleLoginMode();
-    resetErrorMessage();
   };
 
   return {
@@ -96,12 +64,10 @@ export const useLoginViewModel = () => {
     disablePassword,
     displaySpinner,
     errorMessage,
-    onConfirm,
+    onLogin,
     onEnter,
-    onToggleLoginMode,
     setPassword,
     setUsername,
-    loginMode,
     password,
     successMessage,
     username,
