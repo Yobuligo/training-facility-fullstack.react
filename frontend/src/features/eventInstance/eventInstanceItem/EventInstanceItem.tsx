16a77505ab3cfc4024d915cdb97f091ece79f2f7
acoria <tewes.verena@gmail.com>
2024-09-16 09:45:13 +0200

    Adjust training registration header

--- frontend/src/features/eventInstance/eventInstanceItem/EventInstanceItem.tsx
+++ frontend/src/features/eventInstance/eventInstanceItem/EventInstanceItem.tsx
@@ -9,30 +9,39 @@ export const EventInstanceItem: React.FC<IEventInstanceItemProps> = (props) => {
   const renderWeekday = useRenderWeekday();
 
   return (
-    <Card className={styles.eventInstanceItem}>
-      <Banner color={props.eventInstanceItemModel.color} />
-      <div className={styles.body}>
-        <div className={styles.content}>
-          <div className={styles.title}>{props.eventInstanceItemModel.title}</div>
-          <div className={styles.timeContainer}>
-            <div className={styles.weekday}>
-              {renderWeekday(
-                DateTime.toWeekday(props.eventInstanceItemModel.from),
-                true
-              )}
+    <Card className={styles.card}>
+      <div className={styles.eventInstanceItem}>
+        <div className={styles.header}>
+          <Banner color={props.eventInstanceItemModel.color} />
+          <div className={styles.content}>
+            <div className={styles.title}>
+              {props.eventInstanceItemModel.title}
             </div>
-
-            <div className={styles.dateTimeContainer}>
-              <div>
-                {DateTime.format(props.eventInstanceItemModel.from, "dd.MM.yyyy")}
+            <div className={styles.timeContainer}>
+              <div className={styles.weekday}>
+                {renderWeekday(
+                  DateTime.toWeekday(props.eventInstanceItemModel.from),
+                  true
+                )}
               </div>
-              <div>{`${DateTime.toTime(
-                props.eventInstanceItemModel.from
-              )} - ${DateTime.toTime(props.eventInstanceItemModel.to)}`}</div>
+
+              <div className={styles.dateTimeContainer}>
+                <div>
+                  {DateTime.format(
+                    props.eventInstanceItemModel.from,
+                    "dd.MM.yyyy"
+                  )}
+                </div>
+                <div>{`${DateTime.toTime(
+                  props.eventInstanceItemModel.from
+                )} - ${DateTime.toTime(props.eventInstanceItemModel.to)}`}</div>
+              </div>
             </div>
           </div>
         </div>
-        {props.children}
+        {props.children && (
+          <div className={styles.children}>{props.children}</div>
+        )}
       </div>
     </Card>
   );
