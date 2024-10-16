c0b0aacb5292b63e1354ff82406064d3c4125812
Yobuligo <hoffmannpeter82@gmail.com>
2024-10-16 13:53:58 +0200

    Fix bug: Search result list for adding users to a training is not sorted

--- frontend/src/api/UserGradingApi.ts
+++ frontend/src/api/UserGradingApi.ts
@@ -5,11 +5,4 @@ export class UserGradingApi extends Repository<IUserGrading> {
   constructor() {
     super(UserGradingMeta);
   }
-
-  async findByUserId(userId: string): Promise<IUserGrading[]> {
-    // return DummyGradings.filter((grading) => grading.userProfileId === userId).sort(
-    //   (left, right) => DateTime.compare(right.achievedAt, left.achievedAt)
-    // );
-    throw new Error();
-  }
 }
--- frontend/src/features/users/userSearch/userSearchSection/useUserSearchSectionViewModel.ts
+++ frontend/src/features/users/userSearch/userSearchSection/useUserSearchSectionViewModel.ts
@@ -11,6 +11,35 @@ export const useUserSearchSectionViewModel = () => {
     searchRequest(async () => {
       const userApi = new UserApi();
       const users = await userApi.findByQuery(query);
+      users.sort((left, right) => {
+        const userProfileLeft = left.userProfile;
+        const userProfileRight = right.userProfile;
+        if (!userProfileLeft) {
+          return -1;
+        }
+
+        if (!userProfileRight) {
+          return 1;
+        }
+
+        if (userProfileLeft.firstname < userProfileRight.firstname) {
+          return -1;
+        }
+
+        if (userProfileLeft.firstname > userProfileRight.firstname) {
+          return 1;
+        }
+
+        if (userProfileLeft.lastname < userProfileRight.lastname) {
+          return -1;
+        }
+
+        if (userProfileLeft.lastname > userProfileRight.lastname) {
+          return 1;
+        }
+
+        return 0;
+      });
       setUser(users);
     });
   };
