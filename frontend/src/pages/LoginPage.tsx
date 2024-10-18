94274e79fb9d3fec2ff91483f7bb2a7612d392a1
Yobuligo <hoffmannpeter82@gmail.com>
2024-10-18 12:55:35 +0200

    Clicking on the logo on the login page should navigate to dashboard screen

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { PublicPage } from "../components/pages/publicPage/PublicPage";
 import { Login } from "../features/login/Login";
+import { AppRoutes } from "../routes/AppRoutes";
 
 export const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <PublicPage>
+    <PublicPage onAppLogoClick={() => navigate(AppRoutes.dashboard.toPath())}>
       <Login />
     </PublicPage>
   );
