a76ec2c3481e6ce8f1afe1f4da21e252c857f84f
Yobuligo <hoffmannpeter82@gmail.com>
2024-10-22 21:59:41 +0200

    Select always a trainer which is currently not selected

--- frontend/src/components/multiSelectList/useMultiSelectListViewModel.ts
+++ frontend/src/components/multiSelectList/useMultiSelectListViewModel.ts
@@ -11,12 +11,70 @@ export const useMultiSelectListViewModel = <T>(
     IMultiSelectItem<T>[]
   >([{ id: uuid(), options: props.options, selected: props.options[0] }]);
 
+  /**
+   * Returns the keys of the select options, which are in use, which means which are selected.
+   */
+  const findSelectedKeys = (
+    multiSelectItems: IMultiSelectItem<T>[]
+  ): Set<T> => {
+    const selectedKeys = new Set<T>();
+    for (const multiSelectItem of multiSelectItems) {
+      selectedKeys.add(multiSelectItem.selected.key);
+    }
+    return selectedKeys;
+  };
+
+  /**
+   * Returns the first free option, which is not in use, so not selected.
+   */
+  const findFirstFreeOption = (
+    multiSelectItems: IMultiSelectItem<T>[]
+  ): ISelectOption<T> | undefined => {
+    const selectedKeys = findSelectedKeys(multiSelectItems);
+    for (const option of props.options) {
+      if (!selectedKeys.has(option.key)) {
+        return option;
+      }
+    }
+    return undefined;
+  };
+
+  /**
+   * Updates the options of each multiSelectItem. If an option is selected, is must not be available for another selection.
+   */
+  const updateOptions = (multiSelectItems: IMultiSelectItem<T>[]) => {
+    const selectedKeys = findSelectedKeys(multiSelectItems);
+
+    // Update multiSelectItems.options so that already selected elements can not be selected multiple times.
+    // Consider to add the select option, if the selected element of a multiSelectItem is the selected.
+    // E.g. if we have the keys 1, 2, 3 and in one select option, the key 2 is selected, then we have to add the select option with key 2.
+    multiSelectItems.forEach((multiSelectItem) => {
+      for (let i = 0; i < multiSelectItem.options.length; i++) {
+        const option = multiSelectItem.options[i];
+
+        // delete if key is already in use
+        if (
+          option.key !== multiSelectItem.selected.key &&
+          selectedKeys.has(option.key)
+        ) {
+          multiSelectItem.options.splice(i, 1);
+          i--;
+        }
+      }
+    });
+  };
+
   const onAdd = () => {
+    const toBeSelected = findFirstFreeOption(multiSelectItems);
+    if (!toBeSelected) {
+      return;
+    }
+
     setMultiSelectItems((previous) => {
       previous.push({
         id: uuid(),
         options: props.options,
-        selected: props.options[0],
+        selected: toBeSelected,
       });
       return [...previous];
     });
