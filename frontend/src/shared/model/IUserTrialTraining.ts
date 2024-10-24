a55d5820e0dafd9eb56c34caa5a2605317b0f925
Yobuligo <hoffmannpeter82@gmail.com>
2024-10-24 08:24:53 +0200

    Add documentation

--- frontend/src/shared/model/IUserTrialTraining.ts
+++ frontend/src/shared/model/IUserTrialTraining.ts
@@ -3,6 +3,11 @@ import { IRouteMeta } from "../../core/api/types/IRouteMeta";
 import { EventRegistrationState } from "../types/EventRegistrationState";
 import { IHaveName } from "../types/IHaveName";
 
+/**
+ * An instance of this interface represents a trial training
+ * from a specific person with firstname and lastname
+ * for a specific event instance.
+ */
 export interface IUserTrialTraining extends IEntity, IHaveName {
   email: string;
   eventInstanceId: string;
