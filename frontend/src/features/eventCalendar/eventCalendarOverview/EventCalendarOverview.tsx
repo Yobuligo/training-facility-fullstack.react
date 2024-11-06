74ff7df8b0c3a34b9b4d2694ebbd9814d5fd0133
Yobuligo <hoffmannpeter82@gmail.com>
2024-11-06 15:16:19 +0100

    Add padding to EventCalendarOverview

--- frontend/src/features/eventCalendar/eventCalendarOverview/EventCalendarOverview.tsx
+++ frontend/src/features/eventCalendar/eventCalendarOverview/EventCalendarOverview.tsx
@@ -2,6 +2,7 @@ import { TokenRepository } from "../../../api/core/TokenRepository";
 import { EventDefinitionApi } from "../../../api/EventDefinitionApi";
 import { requestToken } from "../../../api/utils/requestToken";
 import { EventCalendarSection } from "../eventCalendarSection/EventCalendarSection";
+import styles from "./EventCalendarOverview.module.scss";
 
 /**
  * This component is responsible for displaying the events for the current week as calendar.
@@ -9,16 +10,18 @@ import { EventCalendarSection } from "../eventCalendarSection/EventCalendarSecti
  */
 export const EventCalendarOverview: React.FC = () => {
   return (
-    <EventCalendarSection
-      eventDefinitionLoader={async (dateTimeSpan) => {
-        TokenRepository.token = await requestToken();
-        const eventDefinitionApi = new EventDefinitionApi();
-        return await eventDefinitionApi.findByDateTimeSpanSecured(
-          dateTimeSpan,
-          true
-        );
-      }}
-      views={["day", "week"]}
-    />
+    <div className={styles.eventCalendarOverview}>
+      <EventCalendarSection
+        eventDefinitionLoader={async (dateTimeSpan) => {
+          TokenRepository.token = await requestToken();
+          const eventDefinitionApi = new EventDefinitionApi();
+          return await eventDefinitionApi.findByDateTimeSpanSecured(
+            dateTimeSpan,
+            true
+          );
+        }}
+        views={["day", "week"]}
+      />
+    </div>
   );
 };
