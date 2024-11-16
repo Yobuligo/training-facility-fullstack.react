9a982c67c481dd46f677d373337660196e7d2c1f
Yobuligo <hoffmannpeter82@gmail.com>
2024-11-16 15:43:07 +0100

    Add test for uuid

--- backend/tests/utils/hash.test.ts
+++ backend/tests/utils/hash.test.ts
@@ -2,17 +2,18 @@ import { expect } from "chai";
 import { hash } from "./../../src/utils/hash";
 
 describe("hash", () => {
+  const value = "hello world";
   it("Returns digest", () => {
-    expect(hash("hello world")).equal(
+    expect(hash(value)).equal(
       "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
     );
   });
 
   it("Returns same digest for same input", () => {
-    expect(hash("hello world")).equal(hash("hello world"));
+    expect(hash(value)).equal(hash(value));
   });
 
   it("Returns different digest for different values", () => {
-    expect(hash("hello world")).not.equal("hello  world");
+    expect(hash(value)).not.equal("hello  world");
   });
 });
