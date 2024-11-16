7ba1c69469ec41d14632fe034a32ff6f728e8999
Yobuligo <hoffmannpeter82@gmail.com>
2024-11-16 15:48:28 +0100

    Add test for ExpiredError

--- backend/tests/utils/hash.test.ts
+++ backend/tests/utils/hash.test.ts
@@ -3,6 +3,7 @@ import { hash } from "./../../src/utils/hash";
 
 describe("hash", () => {
   const value = "hello world";
+
   it("Returns digest", () => {
     expect(hash(value)).equal(
       "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
--- backend/tests/utils/hashPassword.test.ts
+++ backend/tests/utils/hashPassword.test.ts
@@ -4,6 +4,7 @@ import { hashPassword } from "../../src/utils/hashPassword";
 describe("hashPassword", () => {
   const password = "hello world";
   const salt = "123123";
+
   it("Returns hashed password", () => {
     expect(hashPassword(password, salt)).equal(
       "4b64be74a31dc27d6771d2fdae7fa1703f324b61c6620fcdc0f85fdfdc64383e"
