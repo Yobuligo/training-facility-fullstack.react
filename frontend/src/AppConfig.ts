c066bec8b618bff7498e8136281072c6cfa496a1
Yobuligo <hoffmannpeter82@gmail.com>
2024-12-10 10:52:34 +0100

    Add documentation

--- frontend/src/AppConfig.ts
+++ frontend/src/AppConfig.ts
@@ -6,7 +6,7 @@ export const AppConfig = {
     date: "20241129",
     number: 13,
   },
-  defaultDateTimeSpanFilter: "day",
+  defaultDateTimeSpanFilter: "day", // supported values day or week
   HOST:
     process.env.REACT_APP_BACKEND_HOST ??
     error(`Error while getting host information from environment variables`),
