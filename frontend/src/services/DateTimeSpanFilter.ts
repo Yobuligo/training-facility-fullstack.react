7e1461c9e90962d29a4e57413d7ed710864c4307
Yobuligo <hoffmannpeter82@gmail.com>
2025-01-09 15:19:31 +0100

    Update docu

--- frontend/src/services/DateTimeSpanFilter.ts
+++ frontend/src/services/DateTimeSpanFilter.ts
@@ -6,7 +6,7 @@ import { IDateTimeSpanFilter } from "./IDateTimeSpanFilter";
 
 /**
  * This service is responsible for providing the default values for the date time span filter.
- * It is used to define the default *from* and *to* value depending on the big calendar *view*, which might be *day* or *week*.
+ * It is used to define the default *from* and *to* value depending on the big calendar *view*, which might be *day* or *week* and set in the AppConfig.
  */
 export class DateTimeSpanFilter implements IDateTimeSpanFilter {
   get from(): Date {
