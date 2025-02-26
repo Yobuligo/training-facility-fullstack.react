26587a6d65d079623bd7bb1e737c859006435a9a
Yobuligo <hoffmannpeter82@gmail.com>
2025-02-26 09:16:05 +0100

    Display loading spinner for chart only for chart and not page

--- frontend/src/features/stats/StatsSection.tsx
+++ frontend/src/features/stats/StatsSection.tsx
@@ -22,46 +22,44 @@ const StatsSection: React.FC = () => {
   const viewModel = useStatsSectionViewModel();
 
   return (
-    <>
+    <div className={styles.statsSection}>
+      <Toolbar>
+        <SecondaryButton onClick={viewModel.onSelectYear}>
+          {t(texts.calendar.year)}
+        </SecondaryButton>
+        <SecondaryButton onClick={viewModel.onSelectMax}>
+          {t(texts.calendar.max)}
+        </SecondaryButton>
+      </Toolbar>
       {viewModel.isLoadStatsRequestProcessing ? (
         <PageSpinner />
       ) : (
-        <div className={styles.statsSection}>
-          <Toolbar>
-            <SecondaryButton onClick={viewModel.onSelectYear}>
-              {t(texts.calendar.year)}
-            </SecondaryButton>
-            <SecondaryButton onClick={viewModel.onSelectMax}>
-              {t(texts.calendar.max)}
-            </SecondaryButton>
-          </Toolbar>
-          <ResponsiveContainer width="100%" height={"100%"}>
-            <LineChart
-              data={viewModel.chartData?.data}
-              margin={{ left: -25, right: 10, top: 10 }}
-            >
-              <CartesianGrid
-                strokeDasharray="3 3"
-                stroke={colors.colorSecondary}
-              />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
+        <ResponsiveContainer width="100%" height={"100%"}>
+          <LineChart
+            data={viewModel.chartData?.data}
+            margin={{ left: -25, right: 10, top: 10 }}
+          >
+            <CartesianGrid
+              strokeDasharray="3 3"
+              stroke={colors.colorSecondary}
+            />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
 
-              <Line
-                type="monotone"
-                dataKey="value"
-                stroke={colors.colorPrimary}
-                strokeWidth={1}
-                dot={{ fill: colors.colorPrimary, r: 2 }}
-                name={t(texts.stats.activeMembers)}
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
+            <Line
+              type="monotone"
+              dataKey="value"
+              stroke={colors.colorPrimary}
+              strokeWidth={1}
+              dot={{ fill: colors.colorPrimary, r: 2 }}
+              name={t(texts.stats.activeMembers)}
+            />
+          </LineChart>
+        </ResponsiveContainer>
       )}
-    </>
+    </div>
   );
 };
 
