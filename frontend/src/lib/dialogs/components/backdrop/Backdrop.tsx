fd9f8c90f49a988ae7d7604be53ce5caad88824a
Yobuligo <hoffmannpeter82@gmail.com>
2025-03-21 10:07:41 +0100

    Make children in dialog scrollable

--- frontend/src/lib/dialogs/components/backdrop/Backdrop.tsx
+++ frontend/src/lib/dialogs/components/backdrop/Backdrop.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createPortal } from "react-dom";
 import styles from "./Backdrop.module.scss";
 import { IBackdropProps } from "./IBackdropProps";
 
 export const Backdrop: React.FC<IBackdropProps> = (props) => {
+  // Prevent body from scrolling if dialog is open
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return createPortal(
     <div className={styles.backdrop}>{props.children}</div>,
     document.getElementById("backdrop")!
