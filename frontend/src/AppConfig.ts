83030a06d354c94e7c7fa1a87891d7d84b000acf
Yobuligo <hoffmannpeter82@gmail.com>
2025-03-21 11:29:11 +0100

    Build: 1.14.1-20250321.20

--- frontend/src/AppConfig.ts
+++ frontend/src/AppConfig.ts
@@ -2,9 +2,9 @@ import { error } from "./core/utils/error";
 
 export const AppConfig = {
   build: {
-    version: "1.14.0",
+    version: "1.14.1",
     date: "20250321",
-    number: 19,
+    number: 20,
   },
   defaultDateTimeSpanFilter: "day", // Sets the default view for the event calendar (supported values day or week).
   HOST:
